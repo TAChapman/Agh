91d2d843e14c026b8158dae8c13c739bd1699048
Neil <neil.rieck@griffithuni.edu.au>
2013-10-17 18:14:23 +1000

    Added Menu at top with Mnemonics  need to put actions on menu items....

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -1,36 +1,145 @@
 package mines;
 
 import java.awt.BorderLayout;
-
+import java.awt.Event;
+import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Mines extends JFrame {
-
+    //   Window Dimensions pixels
     private final int WIDTH = 960;         //was 250
-    private final int HEIGHT = 660;         //was 290
+    private final int HEIGHT = 660;        //was 290
+    //   Status and Menu setup
+    private JLabel   statusbar;
+    private JMenuBar MinesMenu;      // The menu bar
 
-    private JLabel statusbar;
-    
+    private JMenu gameMenu;          // Game menu    has Options and Exit
+    private JMenu moveMenu;          // Move menu    has Undo and Redo
+    private JMenu aboutMenu;         // About menu   has info
+
+    private JMenuItem optionsItem;   // Options item
+    private JMenuItem exitItem;      // Exit item
+    private JMenuItem undoItem;      // Undo item
+    private JMenuItem redoItem;      // Redo item
+    private JMenuItem infoItem;      // Info item
     public Mines() {
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);         // TODO: close game method
+        buildMenuBar();                  // Menu Added
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
-        setTitle("Minesweeper");
+        setTitle("Find The Pirates");
 
         statusbar = new JLabel("");
-        add(statusbar, BorderLayout.SOUTH);
+        add(statusbar, BorderLayout.NORTH);
+
 
         add(new Board(statusbar));
 
         setResizable(false);
         setVisible(true);
+
+
+
     }
     
     public static void main(String[] args) {
         new Mines();
     }
+
+
+    private void buildMenuBar()
+    {
+        // Create the menu bar.
+        MinesMenu = new JMenuBar();
+
+        //infoItem
+        buildGameMenu();
+        buildMoveMenu();
+        buildAboutMenu();
+
+        //
+        MinesMenu.add(gameMenu);
+        MinesMenu.add(moveMenu);
+        MinesMenu.add(aboutMenu);
+
+        //
+        setJMenuBar(MinesMenu);
+    }
+
+    private void buildGameMenu()
+    {
+        // Create an Options menu item.
+        optionsItem = new JMenuItem("Options");
+        optionsItem.setMnemonic(KeyEvent.VK_O);
+        optionsItem.addActionListener(new ExitListener());
+        // Create an Exit menu item.
+        exitItem = new JMenuItem("Exit");
+        exitItem.setMnemonic(KeyEvent.VK_X);
+        exitItem.addActionListener(new ExitListener());
+
+        // Create a JMenu object for the Game menu.
+        gameMenu = new JMenu("Game");
+        gameMenu.setMnemonic(KeyEvent.VK_G);
+
+        // Add the Exit menu item to the File menu.
+
+        gameMenu.add(optionsItem);
+        gameMenu.add(exitItem);
+    }
+
+
+    private void buildMoveMenu()
+    {
+        //
+        undoItem = new JMenuItem("Undo");
+        undoItem.setMnemonic(KeyEvent.VK_U);
+        undoItem.addActionListener(new ExitListener());
+        //
+        redoItem = new JMenuItem("Redo");
+        redoItem.setMnemonic(KeyEvent.VK_R);
+        redoItem.addActionListener(new ExitListener());
+        //
+        moveMenu = new JMenu("Move");
+        moveMenu.setMnemonic(KeyEvent.VK_M);
+        //
+        moveMenu.add(undoItem);
+        moveMenu.add(redoItem);
+
+    }
+
+
+    private void buildAboutMenu()  {
+
+    infoItem = new JMenuItem("Info");
+    infoItem.setMnemonic(KeyEvent.VK_I);
+    infoItem.addActionListener(new ExitListener());
+    //
+    aboutMenu = new JMenu("About");
+    aboutMenu.setMnemonic(KeyEvent.VK_A);
+    //
+    aboutMenu.add(infoItem);
+
+    }
+
+
+    private class ExitListener implements ActionListener
+    {
+        /**
+         actionPerformed method
+         @param e An ActionEvent object.
+         */
+
+        public void actionPerformed(ActionEvent e)
+        {
+            System.exit(0);
+        }
+    }
+
+
 }
 
